77ebb229d39008ebc6714fdc59bd03e90b7467c8
Emyeeeel <maryamielcatado@gmail.com>
2024-05-15 19:58:31 +0800

    create api services and api provider

---
+++ lib/services/api_services.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/photo_model.dart';
+
+class ApiService {
+  final String _baseUrl = 'https://api.unsplash.com';
+  final String _clientId = 'N__PKKjjE_rGt2fsn4xs_HXE0ajm7pLn5MJxUDMIHCk';
+
+  Future<List<UnsplashPhoto>> getPhotos() async {
+     final response = await http.get(Uri.parse('$_baseUrl/photos/random?count=10&query=wallpaper&orientation=portrait&client_id=$_clientId'));
+     if (response.statusCode == 200) {
+    final List<dynamic> responseData = jsonDecode(response.body);
+    return responseData.map((json) => UnsplashPhoto.fromJson(json)).toList();
+  } else {
+    throw Exception('Failed to load photos');
+  }
+  }
+}
