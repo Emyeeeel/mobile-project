84206dac1a8a7054d9fa20aa3975b6dd869bfe84
Emyeeeel <maryamielcatado@gmail.com>
2024-05-18 00:53:08 +0800

    create fetPhotos method, home page photos provider and ui for home page

--- lib/providers/api_providers.dart
+++ lib/providers/api_providers.dart
@@ -9,3 +9,8 @@ final landingPagePhotosProvider = FutureProvider<List<UnsplashPhoto>>((ref) asyn
   final service = ApiService();
   return service.getPhotosLandingPage();
 });
+
+final homePagePhotosProvider = FutureProvider<List<UnsplashPhoto>>((ref) async {
+  final service = ApiService();
+  return service.fetchPhotos();
+});
