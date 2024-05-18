9a81e65d412815061fab6df3c2b3ef1147d42272
Emyeeeel <maryamielcatado@gmail.com>
2024-05-18 19:11:26 +0800

    added password page as index value to sign up page

--- lib/screens/sign-up/password_page.dart
+++ lib/screens/sign-up/password_page.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:pinterest_clone/screens/sign-up/birthday_page.dart';
-import 'package:pinterest_clone/widgets/button.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pinterest_clone/models/user_model.dart';
 
@@ -14,64 +11,33 @@ class PasswordPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(userProvider);
 
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
-          children: [
-            const SizedBox(height: 35),
-            Text(
-              'Create a password',
-              style: GoogleFonts.inter(
-                color: const Color(0xFF0F0E0F),
-                fontSize: 22,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Container(
-              width: 380,
-              height: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                border: Border.all(width: 1, color: const Color(0xFF0F0E0F)),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: TextField(
-                  controller: passwordController,
-                  style: GoogleFonts.inter(
-                    color: const Color(0xFF0F0E0F),
-                    fontSize: 18,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ),
-            ),
-            const Spacer(),
-            ButtonWidget(
-              text: 'Next',
-              textColor: 'white',
-              buttonColor: 'red',
-              onPressed: () {
-                if (passwordController.text.trim().isNotEmpty) {
-                  ref.read(userProvider.notifier).setPassword(passwordController.text.trim());
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => BirthdayPage(),
-                    ),
-                  );
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Please enter your email')),
-                  );
-                }
-              },
-            ),
-            const SizedBox(height: 20),
-          ],
+    return Column(
+      children: [
+        const Text(
+          'Create a password', 
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
         ),
-      ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width - 40,
+          height: 50,
+          child: CupertinoTextField(
+            obscureText: true,
+              onChanged: (value){
+                ref.read(userProvider.notifier).setPassword(value.trim());
+              },
+              padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+              placeholder: 'Enter your password',
+              placeholderStyle: const TextStyle(color: Color(0xFF8E8E8E)),
+              decoration: BoxDecoration(
+                border: Border.all(width: 2, color: CupertinoColors.black),
+                borderRadius: BorderRadius.circular(16),
+              ),
+          ),
+        ),
+      ],
     );
   }
 }
+
+
