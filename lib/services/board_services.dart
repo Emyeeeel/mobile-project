3c8e1e81ddd97d40dff3c1b1493cdbd5a815321d
Allyssa Echevarria <allyssagrace2003@gmail.com>
2024-05-19 13:51:22 +0800

    display pin done

--- lib/services/board_services.dart
+++ lib/services/board_services.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 class BoardServices{
@@ -54,6 +55,55 @@ class BoardServices{
       print('Board deleted successfully');
     } catch (e) {
       print('Failed to delete board: $e');
+    }
+  }
+
+ Future<List<DocumentSnapshot>> fetchPins(String boardName) async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      print('No user logged in');
+      return [];
+    }
+
+    final userId = user.uid;
+
+    try {
+      final boardSnapshot = await FirebaseFirestore.instance
+          .collection('boards')
+          .where('name', isEqualTo: boardName)
+          .where('user_id', isEqualTo: userId)
+          .get();
+
+      if (boardSnapshot.docs.isEmpty) {
+        print('No board found');
+        return [];
+      }
+
+      final boardId = boardSnapshot.docs.first.id;
+
+      final pinsSnapshot = await FirebaseFirestore.instance
+          .collection('pins')
+          .where('board_id', isEqualTo: boardId)
+          .get();
+
+      return pinsSnapshot.docs;
+    } catch (e) {
+      print('Failed to fetch pins: $e');
+      return [];
+    }
+  }
+
+  Future<String> getImageUrl(String imageUrl) async {
+    if (imageUrl.isEmpty) {
+      return '';
+    }
+
+    try {
+      final downloadUrl = await FirebaseStorage.instance.refFromURL(imageUrl).getDownloadURL();
+      return downloadUrl;
+    } catch (e) {
+      print('Error fetching image from Firebase Storage: $e');
+      return '';
     }
   }
 }
